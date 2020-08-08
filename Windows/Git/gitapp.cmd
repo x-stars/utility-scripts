554064a8bd91ab6bd930b20bd444af5683c51d06
周建华 <zjh96@live.cn>
2020-08-08 19:49:07 +0800

    Add Git Bash scripts.

---
+++ Windows/Git/gitapp.cmd
@@ -0,0 +1,13 @@
+@ REM Run the Git Bash app with the same name.
+@ FOR %%N IN (git.exe
+) DO @ SET GIT_HOME=%%~dp$PATH:N..
+@ SET BASH=%GIT_HOME%\bin\bash.exe
+@ SET PROG=%~n0
+@ FOR /F "delims=" %%A IN (
+    '@ pathtogit.cmd %*'
+) DO @ SET ARGS=%%A
+@ IF "%ARGS%"=="" @ (
+    @ "%BASH%" -c "%PROG%"
+) ELSE @ (
+    @ "%BASH%" -c "%PROG% %ARGS:"=\"%"
+)
---
+++ Windows/Git/pathtogit.cmd
@@ -0,0 +1,34 @@
+@ REM Convert Windows paths in arguments to Git Bash paths.
+@ SETLOCAL ENABLEDELAYEDEXPANSION
+@ SET ARGS=
+@ FOR %%A IN (%*) DO @ (
+    @ SET ARG=%%A
+    @ ECHO !ARG! | @ FINDSTR /V "[\\][\\]" | @ FINDSTR "[\\]" > NUL
+    @ IF "!ERRORLEVEL!"=="0" @ (
+        @ SET ARG=%%~A
+        @ ECHO !ARG! | @ FINDSTR "^[\\]" > NUL
+        @ IF "!ERRORLEVEL!"=="0" @ (
+            @ FOR /F "delims=" %%P IN (
+                '@ ECHO %CD%'
+            ) DO @ SET ARG=%%~dP!ARG!
+        )
+        @ ECHO !ARG! | @ FINDSTR "^[a-zA-Z]:" > NUL
+        @ IF "!ERRORLEVEL!"=="0" @ (
+            @ SET DRV=!ARG:~0,1!
+            @ FOR %%C IN (
+                a b c d e f g h i j k l m
+                n o p q r s t u v w x y z
+            ) DO @ SET DRV=!DRV:%%C=%%C!
+            @ SET ARG=/!DRV!!ARG:~2!
+        )
+        @ SET ARG=!ARG:\=/!
+        @ SET ARG=!ARG:`=\`!
+        @ SET ARG="!ARG!"
+    )
+    @ IF "!ARGS!"=="" @ (
+        @ SET ARGS=!ARG!
+    ) ELSE @ (
+        @ SET ARGS=!ARGS! !ARG!
+    )
+)
+@ IF NOT "!ARGS!"=="" @ ECHO !ARGS!
