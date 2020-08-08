ad35ee7a3b69e5d1279e317f43c09245c60152f2
周建华 <zjh96@live.cn>
2020-08-08 19:48:56 +0800

    Use CMD to convert WSL paths.

--- Windows/WSL/pathtowsl.cmd
+++ Windows/WSL/pathtowsl.cmd
@@ -2,14 +2,28 @@
 @ SETLOCAL ENABLEDELAYEDEXPANSION
 @ SET ARGS=
 @ FOR %%A IN (%*) DO @ (
-    @ SET ARG=%%~A
-    @ ECHO !ARG! | @ FINDSTR "\\" 1>NUL
+    @ SET ARG=%%A
+    @ ECHO !ARG! | @ FINDSTR /V "[\\][\\]" | @ FINDSTR "[\\]" > NUL
     @ IF "!ERRORLEVEL!"=="0" @ (
-        @ SET ARG=!ARG:\=\\!
+        @ SET ARG=%%~A
+        @ ECHO !ARG! | @ FINDSTR "^[\\]" > NUL
+        @ IF "!ERRORLEVEL!"=="0" @ (
+            @ FOR /F "delims=" %%P IN (
+                '@ ECHO %CD%'
+            ) DO @ SET ARG=%%~dP!ARG!
+        )
+        @ ECHO !ARG! | @ FINDSTR "^[a-zA-Z]:" > NUL
+        @ IF "!ERRORLEVEL!"=="0" @ (
+            @ SET DRV=!ARG:~0,1!
+            @ FOR %%C IN (
+                a b c d e f g h i j k l m
+                n o p q r s t u v w x y z
+            ) DO @ SET DRV=!DRV:%%C=%%C!
+            @ SET ARG=/mnt/!DRV!!ARG:~2!
+        )
+        @ SET ARG=!ARG:\=/!
         @ SET ARG=!ARG:`=\`!
-        @ FOR /F "delims=" %%P IN (
-            '@ wsl.exe -- wslpath -- "!ARG!"'
-        ) DO @ SET ARG="%%P"
+        @ SET ARG="!ARG!"
     )
     @ IF "!ARGS!"=="" @ (
         @ SET ARGS=!ARG!
