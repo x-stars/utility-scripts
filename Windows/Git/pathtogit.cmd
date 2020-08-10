da563d467a278bdb62125b5eca77933c3a2602a9
周建华 <zjh96@live.cn>
2020-08-10 23:14:19 +0800

    Fix IF and ECHO in path conversion.

--- Windows/Git/pathtogit.cmd
+++ Windows/Git/pathtogit.cmd
@@ -1,37 +1,37 @@
-@ REM Convert Windows paths in arguments to Git Bash paths.
+@ REM Convert Windows paths in arguments to WSL paths.
 @ SETLOCAL ENABLEDELAYEDEXPANSION
 @ SET ARGS=
-: LOOP ARGS
-    @ IF '%1'=='' @ GOTO ENDLOOP ARGS
+: LOOP_ARGS
+    @ IF '%1'=='' @ GOTO ENDLOOP_ARGS
     @ SET ARG=%1
-    @ ECHO !ARG! | @ FINDSTR /V "[\\][\\]" | @ FINDSTR "[\\]" > NUL
+    @ ECHO:!ARG! | @ FINDSTR /V "[\\][\\]" | @ FINDSTR "[\\]" > NUL
     @ IF !ERRORLEVEL!==0 @ (
         @ SET ARG=%~1
-        @ ECHO !ARG! | @ FINDSTR "^[\\]" > NUL
+        @ ECHO:!ARG! | @ FINDSTR "^[\\]" > NUL
         @ IF !ERRORLEVEL!==0 @ (
             @ FOR /F "delims=" %%P IN (
-                '@ ECHO %CD%'
+                '@ ECHO:%CD%'
             ) DO @ SET ARG=%%~dP!ARG!
         )
-        @ ECHO !ARG! | @ FINDSTR "^[a-zA-Z]:" > NUL
+        @ ECHO:!ARG! | @ FINDSTR "^[a-zA-Z]:" > NUL
         @ IF !ERRORLEVEL!==0 @ (
             @ SET DRV=!ARG:~0,1!
-            @ FOR %%C IN (
+            @ FOR %%D IN (
                 a b c d e f g h i j k l m
                 n o p q r s t u v w x y z
-            ) DO @ SET DRV=!DRV:%%C=%%C!
+            ) DO @ SET DRV=!DRV:%%D=%%D!
             @ SET ARG=/!DRV!!ARG:~2!
         )
         @ SET ARG=!ARG:\=/!
         @ SET ARG=!ARG:`=\`!
         @ SET ARG="!ARG!"
     )
-    @ IF '!ARGS!'=='' @ (
+    @ IF "!ARGS!"=="" @ (
         @ SET ARGS=!ARG!
     ) ELSE @ (
         @ SET ARGS=!ARGS! !ARG!
     )
     @ SHIFT
-    @ GOTO LOOP ARGS
-: ENDLOOP ARGS
-@ IF NOT '!ARGS!'=='' @ ECHO !ARGS!
+    @ GOTO LOOP_ARGS
+: ENDLOOP_ARGS
+@ IF NOT "!ARGS!"=="" @ ECHO:!ARGS!
