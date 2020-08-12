14981aca21ec5a028b56876edbdb7e9e9ff22002
周建华 <zjh96@live.cn>
2020-08-12 21:57:42 +0800

    Add ssh-copy-id CMD script.

---
+++ Windows/ssh-copy-id.cmd
@@ -0,0 +1,26 @@
+@ REM Copy public key of local host to remote host.
+@ IF "%*"=="" @ (
+    @ ECHO usage: %~n0 [-i identity_file] [user@]hostname 1>&2
+    @ EXIT /B 1
+)
+@ SET DEFAULT_KEY=%USERPROFILE%\.ssh\id_rsa
+@ IF "%~1"=="-i" @ (
+    @ SET LOCAL_KEY=%~2
+    @ SET REMOTE_HOST=%~3
+) ELSE @ (
+    @ SET LOCAL_KEY=%DEFAULT_KEY%.pub
+    @ SET REMOTE_HOST=%~1
+)
+@ IF NOT EXIST "%LOCAL_KEY%" @ (
+    @ IF NOT "%~1"=="-i" @ (
+        @ ssh-keygen -t rsa -P "" -f "%DEFAULT_KEY%"
+    )
+)
+@ IF NOT EXIST "%LOCAL_KEY%" @ (
+    @ ECHO error: no identity file in "%LOCAL_KEY%" 1>&2
+    @ EXIT /B 1
+)
+@ SET REMOTE_KEYS=.ssh/authorized_keys
+@ FOR /F "delims=" %%L IN ('@ TYPE "%LOCAL_KEY%"') DO @ (
+    @ ssh %REMOTE_HOST% "echo %%L >> %REMOTE_KEYS%"
+)
