8a18bc24bbbacb247ce6d9c7328114fa8e46ab5f
周建华 <zjh96@live.cn>
2020-09-02 22:14:20 +0800

    Update runas command script.

--- Windows/adminrun.cmd
+++ Windows/adminrun.cmd
@@ -1,1 +1,4 @@
-@ PowerShell.exe -Command Start-Process conhost.exe '%*' -Verb RunAs
+@ REM Run program as Administrator.
+@ SET ARGS=%*
+@ IF DEFINED ARGS @ SET ARGS=%ARGS:'=''%
+@ PowerShell.exe -Command Start-Process conhost.exe '%ARGS%' -Verb RunAs
