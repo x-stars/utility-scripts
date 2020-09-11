c451893d9b393bade5e051224fc72fbb30835057
周建华 <zjh96@live.cn>
2020-09-12 00:07:46 +0800

    Add apt source set script.

---
+++ Linux/apt-set-source.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ARC_SOURCE=archive.ubuntu.com
+SEC_SOURCE=security.ubuntu.com
+
+NEW_SOURCE=$1
+while [ "$NEW_SOURCE" = "" ]
+do read -p "New source = " NEW_SOURCE; done
+
+SOURCE_LIST=/etc/apt/sources.list
+if [ -e "$SOURCE_LIST.org" ]
+then echo "Sources backup exists."; exit 1; fi
+cp $SOURCE_LIST $SOURCE_LIST.org
+
+sed -i "s/$ARC_SOURCE/$NEW_SOURCE/g" $SOURCE_LIST
+sed -i "s/$SEC_SOURCE/$NEW_SOURCE/g" $SOURCE_LIST
