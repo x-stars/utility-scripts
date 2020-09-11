bff0eebce58c0c65d40d7a4e569bf4501ea23afe
周建华 <zjh96@live.cn>
2020-09-12 00:25:24 +0800

    Add Git recursive script.

---
+++ Windows/Git/git-rec.cmd
@@ -0,0 +1,12 @@
+@ REM Run Git command recursively.
+@ FOR /R %%P IN (.) DO @ (
+    @ PUSHD %%~fP
+    @ IF EXIST ".git\" @ (
+        @ ECHO=
+        @ ECHO %%~fP
+        @ ECHO=
+        @ git %*
+        @ ECHO=
+    )
+    @ POPD
+)
