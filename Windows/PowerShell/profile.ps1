e2f99ccbe51df10e62003e92a1646caa0e29e052
周建华 <zjh96@live.cn>
2020-09-12 11:01:04 +0800

    Update PowerShell profile.

--- Windows/PowerShell/profile.ps1
+++ Windows/PowerShell/profile.ps1
@@ -7,13 +7,13 @@
 New-Variable EOL $([System.Environment]::NewLine) -Option Constant
 New-Variable PATHSEP $([System.IO.Path]::PathSeparator) -Option Constant
 
-# 设定环境变量。
-$env:Path = "$env:Path$PATHSEP$(Join-Path $(Split-Path $PROFILE -Parent) Scripts)"
-
 # 设定自定义命令别名。
 New-Alias eval Invoke-Expression
 New-Alias new New-Object
 New-Alias out Out-File
 
-# 引入当前目录的配置文件。
+# 导入脚本文件目录。
+$env:Path = "$env:Path$PATHSEP$(Join-Path $(Split-Path $PROFILE -Parent) Scripts)"
+
+# 导入当前目录的配置文件。
 if (Test-Path ".profile.ps1" -PathType Leaf) { . ".profile.ps1" }
