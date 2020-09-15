e8d6ea2bbcdd2711167342f4172457fd9338b17d
周建华 <zjh96@live.cn>
2020-09-16 00:23:43 +0800

    Fix Maven scripts bug.

--- Windows/Maven/mvn-local.cmd
+++ Windows/Maven/mvn-local.cmd
@@ -3,17 +3,17 @@
 @ SET WORK_DIR=%CD%
 @ SET LOCAL_NAME=settings.xml
 : LOOP_FINDLOCAL
-@ CHDIR ..
 @ SET LOCAL_FILE=!CD!\!LOCAL_NAME!
 @ IF NOT EXIST "!LOCAL_FILE!" @ (
     @ IF NOT "!CD:~-1!"=="\" @ (
+        @ CHDIR ..
         @ GOTO LOOP_FINDLOCAL
     )
 )
 : ENDLOOP_FINDLOCAL
 @ CHDIR %WORK_DIR%
 @ IF EXIST "%LOCAL_FILE%" @ (
-    mvn -s "%LOCAL_FILE%" %*
+    @ mvn -s "%LOCAL_FILE%" %*
 ) ELSE @ (
-    mvn %*
+    @ mvn %*
 )
