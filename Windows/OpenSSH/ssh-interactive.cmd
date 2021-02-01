6ae8196fd721379c2ce13f246c8cb4cf5b9c081c
周建华 <zjh96@live.cn>
2021-02-02 00:59:00 +0800

    Add SSH interactive history.

--- Windows/OpenSSH/ssh-interactive.cmd
+++ Windows/OpenSSH/ssh-interactive.cmd
@@ -1,3 +1,10 @@
-@ REM Input SSH destination interactively.
-@ SET /P DEST=SSH destination: 
-@ ssh %DEST% %*
+@ REM Create SSH connection interactively.
+@ SET KNOWN_DESTS="%UserProfile%\.ssh\known_destinations"
+@ ECHO Known destinations:
+@ TYPE %KNOWN_DESTS% 2>NUL
+@ SET /P DESTINATION=Destination: 
+@ IF NOT DEFINED DESTINATION @ EXIT /B 255
+@ FINDSTR "^%DESTINATION%$" %KNOWN_DESTS% 1>NUL 2>NUL
+@ IF NOT %ERRORLEVEL%==0 @ ECHO:%DESTINATION%>>%KNOWN_DESTS%
+@ ssh %DESTINATION% %*
+@ EXIT /B 0
