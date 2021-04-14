2c0cf25d623f47557906935c9105065f1893478b
周建华 <zjh96@live.cn>
2021-04-14 21:57:11 +0800

    Reformat shell scripts.

--- Linux/apt-set-source.sh
+++ Linux/apt-set-source.sh
@@ -4,12 +4,12 @@ ARC_SOURCE=archive.ubuntu.com
 SEC_SOURCE=security.ubuntu.com
 
 NEW_SOURCE=$1
-while [ "$NEW_SOURCE" = "" ]
-do read -p "New source = " NEW_SOURCE; done
+while [ "$NEW_SOURCE" = "" ]; do
+read -p "New source: " NEW_SOURCE; done
 
 SOURCE_LIST=/etc/apt/sources.list
-if [ -e "$SOURCE_LIST.org" ]
-then echo "Sources backup exists."; exit 1; fi
+if [ -e "$SOURCE_LIST.org" ]; then
+echo "Sources backup exists."; exit 1; fi
 cp $SOURCE_LIST $SOURCE_LIST.org
 
 sed -i "s/$ARC_SOURCE/$NEW_SOURCE/g" $SOURCE_LIST
