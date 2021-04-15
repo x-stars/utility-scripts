9acaab39083dc1642a91f92d7db97ca3afa9002e
周建华 <zjh96@live.cn>
2021-04-15 22:57:11 +0800

    Add PowerShell prompt profile.

--- Windows/PowerShell/profile.ps1
+++ Windows/PowerShell/profile.ps1
@@ -4,19 +4,33 @@
 #>
 
 # 设定自定义全局变量。
-New-Variable NewLine $([System.Environment]::NewLine) -Option Constant
-New-Variable DirSep $([System.IO.Path]::DirectorySeparatorChar) -Option Constant
-New-Variable PathSep $([System.IO.Path]::PathSeparator) -Option Constant
+Set-Variable NewLine $([System.Environment]::NewLine) -Option ReadOnly -Force
+Set-Variable DirSep $([System.IO.Path]::DirectorySeparatorChar) -Option ReadOnly -Force
+Set-Variable PathSep $([System.IO.Path]::PathSeparator) -Option ReadOnly -Force
 
 # 设定自定义命令别名。
-New-Alias eval Invoke-Expression
-New-Alias new New-Object
-New-Alias out Out-File
+Set-Alias eval Invoke-Expression
+Set-Alias new New-Object
+Set-Alias out Out-File
+
+# 定义重新加载配置文件的命令。
+function Update-Profile
+{
+    $PROFILE.PSObject.Properties |
+    Where-Object Name -NE 'Length' |
+    ForEach-Object { $_.Value } |
+    Where-Object { Test-Path $_ -PathType Leaf } |
+    ForEach-Object { . $_ }
+}
 
 # 导入脚本文件目录。
-$env:Path = "$env:Path$PathSep$(Join-Path $(Split-Path $PROFILE -Parent) Scripts)"
+$env:Path = "$env:Path$PathSep$(Join-Path $(Split-Path $PROFILE -Parent) 'Scripts')"
 # 导入当前用户模块。
-Get-ChildItem $(Join-Path $(Split-Path $PROFILE -Parent) 'Modules') -Directory | Import-Module
+Get-ChildItem $(Join-Path $(Split-Path $PROFILE -Parent) Modules) -Directory | Import-Module
+
+# 加载命令提示配置。
+$PromptProfile = $(Join-Path $(Split-Path $PROFILE -Parent) 'prompt.ps1')
+if (Test-Path $PromptProfile -PathType Leaf) { . $PromptProfile }
 
 # 导入当前目录的配置文件。
-if (Test-Path ".profile.ps1" -PathType Leaf) { . ".profile.ps1" }
+if (Test-Path '.profile.ps1' -PathType Leaf) { . '.profile.ps1' }
---
+++ Windows/PowerShell/prompt.ps1
@@ -0,0 +1,21 @@
+﻿<#
+.SYNOPSIS
+当前用户的 PowerShell 命令提示配置。
+#>
+
+function prompt
+{
+    $State = $?
+    $StateText = if ($State) { '(○)' } else { '(×)' }
+    $StateColor = if ($State) { 'Green' } else { 'Red' }
+    Write-Host $StateText -ForegroundColor $StateColor -NoNewline
+    Write-Host "[$($(Get-Date).ToString('HH:mm'))]" -NoNewline
+    Write-Host ' ' -NoNewline
+    Write-Host $env:USERNAME -ForegroundColor Cyan -NoNewline
+    Write-Host '@' -NoNewline
+    Write-Host $env:COMPUTERNAME -ForegroundColor Magenta -NoNewline
+    Write-Host ' ' -NoNewline
+    Write-Host $PWD -ForegroundColor Yellow -NoNewline
+    Write-Host
+    Write-Output '> '
+}
