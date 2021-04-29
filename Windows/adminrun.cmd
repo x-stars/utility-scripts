220a0aac6fab3bdf4b800327bb7a21a47b9ae624
周建华 <zjh96@live.cn>
2021-04-29 23:20:26 +0800

    Simplify PowerShell name in CMD scripts.

--- Windows/adminrun.cmd
+++ Windows/adminrun.cmd
@@ -1,4 +1,4 @@
 @ REM Run program as Administrator.
 @ SET ARGS=%*
 @ IF DEFINED ARGS @ SET ARGS=%ARGS:'=''%
-@ PowerShell.exe -Command Start-Process conhost.exe '%ARGS%' -Verb RunAs
+@ PowerShell -Command Start-Process conhost.exe '%ARGS%' -Verb RunAs
