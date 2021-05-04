f239299fc613966dbcb774982500b85d5e390bd2
周建华 <zjh96@live.cn>
2021-05-04 17:12:01 +0800

    Remove Git proxy scripts.

--- Windows/Git/git-set-proxy.cmd
+++
@@ -1,8 +0,0 @@
-@ SET PROXY=%1
-: SET_PROXY
-@ IF NOT DEFINED PROXY @ (
-    @ SET /P "PROXY=Proxy host:port = "
-    @ GOTO SET_PROXY
-)
-@ git config --global http.proxy "http://%PROXY%"
-@ git config --global https.proxy "https://%PROXY%"
