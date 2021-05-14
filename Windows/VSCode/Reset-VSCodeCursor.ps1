d40658176654b489a838eda586eb235ae6a3e905
周建华 <zjh96@live.cn>
2021-05-15 01:31:44 +0800

    Simplify VS Code cursor scripts.

--- Windows/VSCode/Reset-VSCodeCursor.ps1
+++ Windows/VSCode/Reset-VSCodeCursor.ps1
@@ -1,4 +1,4 @@
-<#
+﻿<#
 .SYNOPSIS
 恢复 Visual Studio Code 的原始指针样式，保持资源文件的完整性。
 #>
@@ -12,7 +12,21 @@ param
 )
 
 # 定义支持多个参数的路径拼接方法。
-function Combine-Path { [System.IO.Path]::Combine([string[]]$args) }
+function Combine-Path
+{
+    [System.IO.Path]::Combine([string[]]$args)
+}
+
+# 定义恢复指定源码文件的方法。
+function Restore-VSCodeSource([string]$Path)
+{
+    $BackupPath = "$Path.org"
+    if (Test-Path $BackupPath)
+    {
+        Remove-Item $Path
+        Move-Item $BackupPath $Path
+    }
+}
 
 # 初始化 Visual Studio Code 相关路径。
 $CodeBinPath = $(Split-Path $(Get-Command code).Source -Parent)
@@ -23,43 +37,23 @@ $SandBoxResDir = Combine-Path code electron-sandbox
 # 初始化主要工作台的 CSS 文件的路径。
 $MainCssName = Combine-Path workbench workbench.desktop.main.css
 $MainCssPath = Combine-Path $VSCodeHome $VSAppResDir $MainCssName
-$MainCssBackupPath = "$MainCssPath.org"
 # 恢复主要工作台的 CSS 文件。
-if (Test-Path $MainCssBackupPath)
-{
-    Remove-Item $MainCssPath
-    Move-Item $MainCssBackupPath $MainCssPath
-}
+Restore-VSCodeSource $MainCssPath
 
 # 初始化主要工作台的 JS 文件的路径。
 $MainJsName = Combine-Path workbench workbench.desktop.main.js
 $MainJsPath = Combine-Path $VSCodeHome $VSAppResDir $MainJsName
-$MainJsBackupPath = "$MainJsPath.org"
 # 恢复主要工作台的 JS 文件。
-if (Test-Path $MainJsBackupPath)
-{
-    Remove-Item $MainJsPath
-    Move-Item $MainJsBackupPath $MainJsPath
-}
+Restore-VSCodeSource $MainJsPath
 
 # 初始化报告问题窗口的 CSS 文件的路径。
 $ReportCssName = Combine-Path $SandBoxResDir issue issueReporterMain.css
 $ReportCssPath = Combine-Path $VSCodeHome $VSAppResDir $ReportCssName
-$ReportCssBackupPath = "$ReportCssPath.org"
 # 恢复报告问题窗口的 CSS 文件。
-if (Test-Path $ReportCssBackupPath)
-{
-    Remove-Item $ReportCssPath
-    Move-Item $ReportCssBackupPath $ReportCssPath
-}
+Restore-VSCodeSource $ReportCssPath
 
 # 初始化进程查看窗口的 CSS 文件的路径。
 $ProcessCssName = Combine-Path $SandBoxResDir processExplorer processExplorerMain.css
 $ProcessCssPath = Combine-Path $VSCodeHome $VSAppResDir $ProcessCssName
-$ProcessCssBackupPath = "$ProcessCssPath.org"
 # 恢复进程查看窗口的 CSS 文件。
-if (Test-Path $ProcessCssBackupPath)
-{
-    Remove-Item $ProcessCssPath
-    Move-Item $ProcessCssBackupPath $ProcessCssPath
-}
+Restore-VSCodeSource $ProcessCssPath
--- Windows/VSCode/Set-VSCodeCursor.ps1
+++ Windows/VSCode/Set-VSCodeCursor.ps1
@@ -1,4 +1,4 @@
-<#
+﻿<#
 .SYNOPSIS
 设置 Visual Studio Code 的指针样式，使其符合原生应用的样式。
 #>
@@ -12,84 +12,73 @@ param
 )
 
 # 定义支持多个参数的路径拼接方法。
-function Combine-Path { [System.IO.Path]::Combine([string[]]$args) }
+function Combine-Path
+{
+    [System.IO.Path]::Combine([string[]]$args)
+}
+
+# 定义更新并备份指定源码文件的方法。
+function Update-VSCodeSource([string]$Path, [string]$Content)
+{
+    $Encoding = [System.Text.UTF8Encoding]::new($false)
+    $BackupPath = "$Path.org"
+    if (-not $(Test-Path $BackupPath))
+    {
+        Move-Item $Path $BackupPath
+    }
+    [System.IO.File]::WriteAllText($Path, $Content, $Encoding)
+}
 
 # 初始化 Visual Studio Code 相关路径。
 $CodeBinPath = $(Split-Path $(Get-Command code).Source -Parent)
 $VSCodeHome = $(Get-Item $(Join-Path $CodeBinPath ..)).FullName
 $VSAppResDir = Combine-Path resources app out vs
 $SandBoxResDir = Combine-Path code electron-sandbox
-# 定义相关文件的输出编码。
-$Encoding = [System.Text.UTF8Encoding]::new($false)
 
 # 初始化主要工作台的 CSS 文件的路径。
 $MainCssName = Combine-Path workbench workbench.desktop.main.css
 $MainCssPath = Combine-Path $VSCodeHome $VSAppResDir $MainCssName
-$MainCssBackupPath = "$MainCssPath.org"
 # 修改并输出主要工作台的 CSS 文件。
 $MainCssText = [System.IO.File]::ReadAllText($MainCssPath)
-$MainCssText = $MainCssText.Replace('cursor:pointer', 'cursor:default')
-$MainCssText = $MainCssText.Replace('.title{cursor:grab}', '.title{cursor:default}')
-$MainCssText = $MainCssText.Replace('.pointer{cursor:default}', '.pointer{cursor:pointer}')
-$MainCssText = $MainCssText.Replace(
-    '.action-menu-item{height:1.8em}', '.action-menu-item{height:1.8em;cursor:default}')
-$MainCssText = $MainCssText.Replace(
-    '.detected-link-active{cursor:default}', '.detected-link-active{cursor:pointer}')
-$MainCssText = $MainCssText.Replace(
-    '.button-link:hover{background:transparent}',
-    '.button-link:hover{background:transparent;cursor:pointer}')
-$MainCssText = $MainCssText.Replace(
-    '.codelens-decoration>a:hover{cursor:default}',
-    '.codelens-decoration>a:hover{cursor:pointer}')
-$MainCssText = $MainCssText.Replace(
-    '.goto-definition-link{text-decoration:underline;cursor:default}',
-    '.goto-definition-link{text-decoration:underline;cursor:pointer}')
-$MainCssText = $MainCssText.Replace(
-    '.button-link{padding:0;background:transparent;margin:2px;cursor:default;',
-    '.button-link{padding:0;background:transparent;margin:2px;cursor:pointer;')
-if (-not $(Test-Path $MainCssBackupPath))
-{
-    Move-Item $MainCssPath $MainCssBackupPath
-}
-[System.IO.File]::WriteAllText($MainCssPath, $MainCssText, $Encoding)
+$MainCssText = $MainCssText.Replace('cursor:pointer', 'cursor:default').
+    Replace('.title{cursor:grab}', '.title{cursor:default}').
+    Replace('.pointer{cursor:default}', '.pointer{cursor:pointer}').
+    Replace('.action-menu-item{height:1.8em}',
+            '.action-menu-item{height:1.8em;cursor:default}').
+    Replace('.detected-link-active{cursor:default}',
+            '.detected-link-active{cursor:pointer}').
+    Replace('.button-link:hover{background:transparent}',
+            '.button-link:hover{background:transparent;cursor:pointer}').
+    Replace('.codelens-decoration>a:hover{cursor:default}',
+            '.codelens-decoration>a:hover{cursor:pointer}').
+    Replace('.goto-definition-link{text-decoration:underline;cursor:default}',
+            '.goto-definition-link{text-decoration:underline;cursor:pointer}').
+    Replace('.button-link{padding:0;background:transparent;margin:2px;cursor:default;',
+            '.button-link{padding:0;background:transparent;margin:2px;cursor:pointer;')
+Update-VSCodeSource $MainCssPath $MainCssText
 
 # 初始化主要工作台的 JS 文件的路径。
 $MainJsName = Combine-Path workbench workbench.desktop.main.js
 $MainJsPath = Combine-Path $VSCodeHome $VSAppResDir $MainJsName
-$MainJsBackupPath = "$MainJsPath.org"
 # 修改并输出主要工作台的 JS 文件。
 $MainJsText = [System.IO.File]::ReadAllText($MainJsPath)
-$MainJsText = $MainJsText.Replace(
-    ".action-menu-item {`n`tflex: 1 1 auto;",
-    ".action-menu-item {`n`tcursor:default;`n`tflex: 1 1 auto;")
-if (-not $(Test-Path $MainJsBackupPath))
-{
-    Move-Item $MainJsPath $MainJsBackupPath
-}
-[System.IO.File]::WriteAllText($MainJsPath, $MainJsText, $Encoding)
+$MainJsText = $MainJsText.
+    Replace(".action-menu-item {`n`tflex: 1 1 auto;",
+            ".action-menu-item {`n`tcursor:default;`n`tflex: 1 1 auto;")
+Update-VSCodeSource $MainJsPath $MainJsText
 
 # 初始化报告问题窗口的 CSS 文件的路径。
 $ReportCssName = Combine-Path $SandBoxResDir issue issueReporterMain.css
 $ReportCssPath = Combine-Path $VSCodeHome $VSAppResDir $ReportCssName
-$ReportCssBackupPath = "$ReportCssPath.org"
 # 修改并输出报告问题窗口的 CSS 文件。
 $ReportCssText = [System.IO.File]::ReadAllText($ReportCssPath)
 $ReportCssText = $ReportCssText.Replace('cursor:pointer', 'cursor:default')
-if (-not $(Test-Path $ReportCssBackupPath))
-{
-    Move-Item $ReportCssPath $ReportCssBackupPath
-}
-[System.IO.File]::WriteAllText($ReportCssPath, $ReportCssText, $Encoding)
+Update-VSCodeSource $ReportCssPath $ReportCssText
 
 # 初始化进程查看窗口的 CSS 文件的路径。
 $ProcessCssName = Combine-Path $SandBoxResDir processExplorer processExplorerMain.css
 $ProcessCssPath = Combine-Path $VSCodeHome $VSAppResDir $ProcessCssName
-$ProcessCssBackupPath = "$ProcessCssPath.org"
 # 修改并输出进程查看窗口的 CSS 文件。
 $ProcessCssText = [System.IO.File]::ReadAllText($ProcessCssPath)
 $ProcessCssText = $ProcessCssText.Replace('cursor:pointer', 'cursor:default')
-if (-not $(Test-Path $ProcessCssBackupPath))
-{
-    Move-Item $ProcessCssPath $ProcessCssBackupPath
-}
-[System.IO.File]::WriteAllText($ProcessCssPath, $ProcessCssText, $Encoding)
+Update-VSCodeSource $ProcessCssPath $ProcessCssText
