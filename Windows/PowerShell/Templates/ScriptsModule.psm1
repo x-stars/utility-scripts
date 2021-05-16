b0b4c38d3a0bc1910c0c5afcf49618b8a93f2e2c
周建华 <zjh96@live.cn>
2021-05-16 19:42:42 +0800

    Update PowerShell scripts module template.

--- Windows/PowerShell/Templates/ScriptsModule.psm1
+++ Windows/PowerShell/Templates/ScriptsModule.psm1
@@ -3,6 +3,23 @@
 提供以脚本文件为基础的 PowerShell 模块定义的模板。
 #>
 
-Get-Item $(Join-Path $PSScriptRoot '*.ps1') |
-Where-Object { Test-Path $_.FullName -PathType Leaf } |
-ForEach-Object { Set-Item Function:\$($_.BaseName) $(Get-Content $_.FullName -Raw) }
+Get-ChildItem $PSScriptRoot '*.ps1' -File | ForEach-Object `
+{
+    $Content = Get-Content $_.FullName
+    $NewLine = [System.Environment]::NewLine
+    $RawContent = $Content -join $NewLine
+    $SynopsisPattern = @('<#', '\.SYNOPSIS', '(.|', ')*', '#>') -join $NewLine
+    $SynopsisRegex = [regex]::new($SynopsisPattern, 'IgnoreCase, Multiline')
+    $Synopsis = $SynopsisRegex.Match($RawContent).Value
+    $ScriptAst = [scriptblock]::Create($RawContent).Ast
+    $UsingContent = $ScriptAst.UsingStatements -join $NewLine
+    $FunctionHead = @($Synopsis, '', $UsingContent) -join $NewLine
+    $AttrsContent = $ScriptAst.ParamBlock.Attributes -join $NewLine
+    $FunctionParam = @($AttrsContent, $ScriptAst.ParamBlock) -join $NewLine
+    $FunctionBody = @('process', '{', "    & ""$($_.FullName)"" @args", '}') -join $NewLine
+    $FunctionContent = @($FunctionHead, '', $FunctionParam, '', $FunctionBody) -join $NewLine
+    $FunctionName = $_.BaseName
+    Set-Item Function:\$FunctionName $FunctionContent
+    $Content | Where-Object { $_ -imatch '^\[Alias\([''"].+[''"]\)\]$' } |
+    ForEach-Object { Set-Alias $_.Substring(8, $_.Length - 11) $FunctionName }
+}
