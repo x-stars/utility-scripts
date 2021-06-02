6b8952f2374ad93fd107f637385d7f2e61e427f2
周建华 <zjh96@live.cn>
2021-06-02 22:30:23 +0800

    Normalize stderr redirect in CMD scripts.

--- Windows/OpenSSH/ssh-copy-id.cmd
+++ Windows/OpenSSH/ssh-copy-id.cmd
@@ -1,7 +1,7 @@
 @ REM Copy public key of local host to remote host.
 @ SET ARGS=%*
 @ IF NOT DEFINED ARGS @ (
-    @ ECHO usage: %~n0 [-i identity_file] [user@]hostname 1>&2
+    @ ECHO>&2 usage: %~n0 [-i identity_file] [user@]hostname
     @ EXIT /B 255
 )
 @ SET DEFAULT_KEY=%USERPROFILE%\.ssh\id_rsa
@@ -18,10 +18,10 @@
     )
 )
 @ IF NOT EXIST "%LOCAL_KEY%" @ (
-    @ ECHO error: no identity file in "%LOCAL_KEY%" 1>&2
+    @ ECHO>&2 error: no identity file in "%LOCAL_KEY%"
     @ EXIT /B 255
 )
 @ SET REMOTE_KEYS=.ssh/authorized_keys
 @ FOR /F "delims=" %%L IN ('@ TYPE "%LOCAL_KEY%"') DO @ (
-    @ ssh %REMOTE_HOST% "echo %%L >> %REMOTE_KEYS%"
+    @ ssh %REMOTE_HOST% "echo>>%REMOTE_KEYS% %%L"
 )
