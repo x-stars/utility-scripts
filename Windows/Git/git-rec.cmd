fa583f18a49c958475c6f332df47af2cc845fa12
周建华 <zjh96@live.cn>
2021-07-12 22:52:20 +0800

    Update Git recursive script.

--- Windows/Git/git-rec.cmd
+++ Windows/Git/git-rec.cmd
@@ -1,7 +1,7 @@
 @ REM Run Git command recursively.
 @ FOR /R %%P IN (.) DO @ (
-    @ IF EXIST "%%~fP\.git\" @ (
-        @ PUSHD "%%~fP"
+    @ IF EXIST "%%~fP\.git" @ (
+        @ PUSHD %%~fP
         @ ECHO [35m%%~fP[0m
         @ git %*
         @ ECHO=
