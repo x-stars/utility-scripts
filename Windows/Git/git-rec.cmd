ddcef8ecbc8bbeb447be6c87ebfd926cd66dfaf9
周建华 <zjh96@live.cn>
2021-07-27 23:20:41 +0800

    Add exit code & message to git-rec.

--- Windows/Git/git-rec.cmd
+++ Windows/Git/git-rec.cmd
@@ -8,17 +8,19 @@
 : MAIN
 @ SET CD=
 @ CALL:LENGTH
+@ SET TOTALERROR=0
 @ SET PT=SET /P ^<NUL:
 @ FOR /R %%P IN (.) DO @ (
     @ IF EXIST "%%~fP\.git" @ (
         @ PUSHD %%~fP
         @ CALL:HEADER
         @ git %*
+        @ CALL:STATUS
         @ ECHO=
         @ POPD
     )
 )
-@ EXIT /B %ERRORLEVEL%
+@ EXIT /B %TOTALERROR%
 
 : LENGTH
 @ SET  RLEN=0
@@ -41,4 +43,13 @@
 @ %PT%=">>> "& %PT%>CON:=[35m
 @ SET /P <NUL:="%REPO%"
 @ %PT%>CON:=[0m& ECHO= ^<^<^<
+@ EXIT /B %ERRORLEVEL%
+
+: STATUS
+@ SET EXITCODE=%ERRORLEVEL%
+@ SET /A TOTALERROR += %EXITCODE%
+@ IF %EXITCODE% == 0 @ EXIT /B 0
+@ %PT%>&2=!!! & %PT%>CON:=[31m
+@ %PT%>&2=exit with code %EXITCODE%
+@ %PT%>CON:=[0m& ECHO>&2= !!!
 @ EXIT /B %ERRORLEVEL%
