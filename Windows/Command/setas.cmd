f353e0bae8d801e7872d7ad45af58a06bcc53abc
周建华 <zjh96@live.cn>
2021-07-30 22:23:07 +0800

    Add ERRORLEVEL support for SETAS.

--- Windows/Command/setas.cmd
+++ Windows/Command/setas.cmd
@@ -1,10 +1,15 @@
 @ REM Set variable as command output.
-@ SETLOCAL ENABLEEXTENSIONS & SET ERRORLEVEL=
+@ SETLOCAL ENABLEEXTENSIONS
+@ SET ERRORLEVEL=& SET RANDOM=
 @ SET "VARNAME=%~1" & SET "COMMAND=%~2" & SET REMAINS=%3
 @ IF "%VARNAME%" == "/?" @ CALL:HELPINFO & EXIT /B 0
 @ IF NOT DEFINED VARNAME @ CALL:INVLDSYN & EXIT /B 1
 @ IF NOT DEFINED COMMAND @ CALL:INVLDSYN & EXIT /B 1
 @ IF     DEFINED REMAINS @ CALL:INVLDSYN & EXIT /B 1
+@ IF DEFINED SETASERR (GOTO TEMPFILE) ELSE (GOTO PIPELINE)
+
+: PIPELINE
+@ REM Output to pipeline without exit code.
 @ ENDLOCAL & SET "%VARNAME%=" & SET "%VARNAME%#0=0" & (
     @ FOR /F "usebackq tokens=1,* delims=:" %%L IN (
         `" (%COMMAND%) | FINDSTR /N .* "`
@@ -12,8 +17,19 @@
 ) & ( CALL SET "%VARNAME%=%%%VARNAME%#1%%" ) ^
 & EXIT /B %ERRORLEVEL%
 
+: TEMPFILE
+@ REM Output to temporary file with exit code.
+@ SET OUTFILE=%TEMP%\SETAS-%VARNAME%#%RANDOM%.txt
+@ (%COMMAND%)>"%OUTFILE%" & CALL SET EXITCODE=%%ERRORLEVEL%%
+@ ENDLOCAL & SET "%VARNAME%=" & SET "%VARNAME%#0=0" & (
+    @ FOR /F "usebackq tokens=1,* delims=:" %%L IN (
+        `@ FINDSTR /N .* "%OUTFILE%"`
+    ) DO @ SET "%VARNAME%#0=%%L" & SET "%VARNAME%#%%L=%%M"
+) & ( CALL SET "%VARNAME%=%%%VARNAME%#1%%" ) ^
+& DEL "%OUTFILE%" & EXIT /B %EXITCODE%
+
 : INVLDSYN
-@ ECHO=The syntax of the command is incorrect.
+@ ECHO>&2=The syntax of the command is incorrect.
 @ EXIT /B %ERRORLEVEL%
 
 : HELPINFO
@@ -24,6 +40,8 @@
 @ ECHO=& ECHO=%CMDNAME% variable="command"& ECHO=
 @ ECHO=  variable  Specifies the variable name to set.
 @ ECHO=  command   Specifies the command to execute.
+@ ECHO=& ECHO=If variable %%SETASERR%% is set to any value,
+@ ECHO=%CMDNAME% will set the ERRORLEVEL of the command.
 @ ECHO=& ECHO=These variables will also set:& ECHO=
 @ ECHO=  variable#0  Represents the lines of output.
 @ ECHO=  variable#n  Represents the n-th line of output.
