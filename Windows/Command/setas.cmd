dd16018be4ae9f1090d5d3265b0ae77184c68b22
周建华 <zjh96@live.cn>
2021-07-31 21:38:32 +0800

    Optimize SETAS help message.

--- Windows/Command/setas.cmd
+++ Windows/Command/setas.cmd
@@ -33,15 +33,17 @@
 @ EXIT /B %ERRORLEVEL%
 
 : HELPINFO
-@ SET CMDNAME=%~n0& FOR %%L IN (
+@ SET PT=SET /P ^<NUL:
+@ SET "CMDNAME=%~n0" & FOR %%L IN (
     A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
-) DO @ CALL SET CMDNAME=%%CMDNAME:%%L=%%L%%
+) DO @ CALL SET "CMDNAME=%%CMDNAME:%%L=%%L%%"
 @ ECHO=Sets a variable value as output of a command.
-@ ECHO=& ECHO=%CMDNAME% variable="command"& ECHO=
+@ ECHO=& %PT%="%CMDNAME%"& EHO= variable="command"& ECHO=
 @ ECHO=  variable  Specifies the variable name to set.
 @ ECHO=  command   Specifies the command to execute.
 @ ECHO=& ECHO=If variable %%SETASERR%% is set to any value,
-@ ECHO=%CMDNAME% will set the ERRORLEVEL of the command.
+@ %PT%="%CMDNAME%"& ECHO= will set ERRORLEVEL of the command.
+@ ECHO=NOTE: This will slow down the command execution.
 @ ECHO=& ECHO=These variables will also set:& ECHO=
 @ ECHO=  variable#0  Represents the lines of output.
 @ ECHO=  variable#n  Represents the n-th line of output.
