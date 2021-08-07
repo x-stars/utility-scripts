0997311bdeaba21722818f3a633509f0a5fb0d2a
周建华 <zjh96@live.cn>
2021-08-08 02:02:49 +0800

    Fix PowerShell Foreach-Object usage.

--- Windows/Git/git-prec.ps1
+++ Windows/Git/git-prec.ps1
@@ -25,9 +25,9 @@ Receive-Job -Wait -AutoRemoveJob | ForEach-Object `
     [System.Console]::ForegroundColor = 'Magenta'
     Write-Output "REPO: $($_.Repository)"
     [System.Console]::ResetColor()
-    $_.GitOutput | ForEach-Object { Write-Output $_ }
-    $_.GitError | ForEach-Object { Write-Warning $_ }
+    foreach ($Line in $_.GitOutput) { Write-Output $Line }
+    foreach ($Line in $_.GitError) { Write-Warning $Line }
     $ExitMessage = 'exit with code {0}' -f $_.ExitCode
-    if ($_.ExitCode) { Write-Warning $ExitMessage }
+    if ($_.ExitCode -ne 0) { Write-Warning $ExitMessage }
     Write-Output ''
 }
