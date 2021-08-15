705bedf91cfd6b0fe4529398f56f92ed4511a533
周建华 <zjh96@live.cn>
2021-08-15 10:25:55 +0800

    Avoid using stdin to pass commands.

--- Windows/PowerShell/Templates/pscmds.cmd
+++ Windows/PowerShell/Templates/pscmds.cmd
@@ -1,23 +1,20 @@
 @ REM Invoke PowerShell commands below : POWERSHELL_COMMANDS.
 @ REM NOTE: This script must be encoded with UTF8 without BOM.
-@ SETLOCAL ENABLEEXTENSIONS
-@ SET "PSCMDPATH=%~f0"
-@ SET "PT=SET /P <NUL:" & SET "NL=ECHO="
-@ SET "ERRORLEVEL=" & (
-    %PT%="$PSCmdCommandPath = $env:PSCMDPATH"&%NL%
-    %PT%="$PSCmdScriptRoot = $(Split-Path $PSCmdCommandPath -Parent)"&%NL%
-    %PT%="$PSDefaultParameterValues['*:Encoding'] = 'UTF8'"&%NL%
-    %PT%="$OutputEncoding = [System.Text.UTF8Encoding]::new($false)"&%NL%
-    %PT%="[System.Console]::InputEncoding = $OutputEncoding"&%NL%
-    %PT%="[System.Console]::OutputEncoding = $OutputEncoding"&%NL%
-    %PT%="$PSCmdContent = $(Get-Content -LiteralPath $PSCmdCommandPath)"&%NL%
-    %PT%="$PSCmdIndex = $PSCmdContent.IndexOf(': POWERSHELL_COMMANDS')"&%NL%
-    %PT%="$PSCmdNoLabel = 'Label : POWERSHELL_COMMANDS not found.'"&%NL%
-    %PT%="if ($PSCmdIndex -lt 0) { Write-Error $PSCmdNoLabel; exit 1 }"&%NL%
-    %PT%="$PSCmdRange = $($PSCmdIndex + 1)..$($PSCmdContent.Length)"&%NL%
-    %PT%="$PSCmdContent = $($PSCmdContent[$PSCmdRange] | Out-String)"&%NL%
-    %PT%="& $([scriptblock]::Create($PSCmdContent)) "&ECHO=%*
-) | PowerShell -Command -
+@ SETLOCAL ENABLEEXTENSIONS & SET "ERRORLEVEL="
+@ SET "PSCMDPATH=%~f0" & SET "PSCMDLINE=$PSCmdCommandPath = $env:PSCMDPATH"
+@ SET "PSCMDLINE=%PSCMDLINE%; $PSCmdScriptRoot = $(Split-Path $PSCmdCommandPath -Parent)"
+@ SET "PSCMDLINE=%PSCMDLINE%; $PSDefaultParameterValues['*:Encoding'] = 'UTF8'"
+@ SET "PSCMDLINE=%PSCMDLINE%; $OutputEncoding = [System.Text.UTF8Encoding]::new($false)"
+@ SET "PSCMDLINE=%PSCMDLINE%; [System.Console]::InputEncoding = $OutputEncoding"
+@ SET "PSCMDLINE=%PSCMDLINE%; [System.Console]::OutputEncoding = $OutputEncoding"
+@ SET "PSCMDLINE=%PSCMDLINE%; $PSCmdContent = $(Get-Content -LiteralPath $PSCmdCommandPath)"
+@ SET "PSCMDLINE=%PSCMDLINE%; $PSCmdIndex = $PSCmdContent.IndexOf(': POWERSHELL_COMMANDS')"
+@ SET "PSCMDLINE=%PSCMDLINE%; $PSCmdNoLabel = 'Label : POWERSHELL_COMMANDS not found.'"
+@ SET "PSCMDLINE=%PSCMDLINE%; if ($PSCmdIndex -lt 0) { Write-Error $PSCmdNoLabel; exit 1 }"
+@ SET "PSCMDLINE=%PSCMDLINE%; $PSCmdRange = $($PSCmdIndex + 1)..$($PSCmdContent.Length)"
+@ SET "PSCMDLINE=%PSCMDLINE%; $PSCmdContent = $($PSCmdContent[$PSCmdRange] | Out-String)"
+@ SET "PSCMDLINE=%PSCMDLINE%; & $([scriptblock]::Create($PSCmdContent)) %*"
+@ PowerShell -Command "%PSCMDLINE%"
 @ EXIT /B %ERRORLEVEL%
 
 @ REM Write PowerShell commands below : POWERSHELL_COMMANDS.
