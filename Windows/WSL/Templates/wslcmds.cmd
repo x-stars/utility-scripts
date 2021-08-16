fa22040adda740eb39acf547249c825e2b8c6891
周建华 <zjh96@live.cn>
2021-08-17 00:16:02 +0800

    Fix exit code of WSL CMD wrapper.

--- Windows/WSL/Templates/wslcmds.cmd
+++ Windows/WSL/Templates/wslcmds.cmd
@@ -2,7 +2,7 @@
 @ REM NOTE: This script must be encoded with UTF8 without BOM.
 @ SETLOCAL ENABLEEXTENSIONS
 @ SET "ERRORLEVEL=" & CHCP 65001 1>NUL:
-@ SET "WSLCMDARGS=%*"
+@ SET "WSLCMDNAME=%~n0" & SET "WSLCMDARGS=%*"
 @ IF DEFINED WSLCMDARGS @ SET "WSLCMDARGS=%WSLCMDARGS:\=\\%"
 @ IF DEFINED WSLCMDARGS @ SET "WSLCMDARGS=%WSLCMDARGS:"=\"%"
 @ SET "WSLCMDPATH=%~f0" & SET "WSLCMDINDEX="
@@ -15,15 +15,16 @@
 @ SET "SHELL=/bin/sh"
 @ FOR /F "tokens=1,* delims=:#!" %%L IN (
     '@ FINDSTR /BN /C:"#!" "%WSLCMDPATH%"'
-) DO @ IF %WSLCMDINDEX% == %%L (SET "SHELL=%%M") & GOTO BREAK_FINDSHELL
+) DO @ IF %WSLCMDINDEX% == %%L @ SET "SHELL=%%M" & GOTO BREAK_FINDSHELL
 : BREAK_FINDSHELL
-@ SET "WSLCMDLINE=export WSLCMDPATH=`wslpath '%WSLCMDPATH:'='\''%'`"
-@ SET "WSLCMDLINE=%WSLCMDLINE% ; wslpath '%WSLCMDPATH:'='\''%'"
-@ SET "WSLCMDLINE=%WSLCMDLINE% | xargs tail -n+%WSLCMDINDEX%"
-@ SET "WSLCMDLINE=%WSLCMDLINE% | sed 's/\r$//'" & REM dos2unix
-@ SET "WSLCMDLINE=%WSLCMDLINE% > /tmp/wslcmds.$$.sh"
-@ SET "WSLCMDLINE=%WSLCMDLINE% ; %SHELL% /tmp/wslcmds.$$.sh %WSLCMDARGS%"
-@ SET "WSLCMDLINE=%WSLCMDLINE% ; rm /tmp/wslcmds.$$.sh"
+@ SET "#1=export WSLCMDPATH=`wslpath '%WSLCMDPATH:'='\''%'`"
+@ SET "#2=WSLCMDTEMP='/tmp/%WSLCMDNAME:'='\''%.'$$'.shcmd'"
+@ SET "#3=tail -n+%WSLCMDINDEX% \"$WSLCMDPATH\""
+@ SET "#4=sed 's/\r$//' > \"$WSLCMDTEMP\""
+@ SET "#5=%SHELL% \"$WSLCMDTEMP\" %WSLCMDARGS%"
+@ SET "#6=EXITCODE=$?; rm \"$WSLCMDTEMP\"; exit $EXITCODE"
+@ SET "WSLCMDLINE=%#1%; %#2%; %#3% | %#4%; %#5%; %#6%"
+@ SET #1=& SET #2=& SET #3=& SET #4=& SET #5=& SET #6=
 @ wsl.exe --exec /bin/sh -c "%WSLCMDLINE%"
 @ EXIT /B %ERRORLEVEL%
 
