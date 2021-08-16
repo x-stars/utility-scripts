6ab759942792f303a11aae2e1b81a0f568f7fed2
周建华 <zjh96@live.cn>
2021-08-16 23:01:20 +0800

    Add comment help and pipeline support.

--- Windows/PowerShell/Templates/pscmds.cmd
+++ Windows/PowerShell/Templates/pscmds.cmd
@@ -1,7 +1,8 @@
 @ REM Invoke PowerShell commands below : POWERSHELL_COMMANDS.
 @ REM NOTE: This script must be encoded with UTF8 without BOM.
 @ SETLOCAL ENABLEEXTENSIONS 
-@ SET "ERRORLEVEL=" & SET "PSCMDPATH=%~f0" & SET "PSCMDARGS=%*"
+@ SET "ERRORLEVEL=" & SET "PSCMDARGS=%*"
+@ SET "PSCMDNAME=%~n0" & SET "PSCMDPATH=%~f0"
 @ IF DEFINED PSCMDARGS @ SET "PSCMDARGS=%PSCMDARGS:"=\"%"
 @ SET "#1=$PSCmdCommandPath = $env:PSCMDPATH"
 @ SET "#2=$PSCmdScriptRoot = $(Split-Path $PSCmdCommandPath -Parent)"
@@ -15,11 +16,13 @@
 @ SET "#10=if ($PSCmdIndex -lt 0) { Write-Error $PSCmdNoLabel; exit 1 }"
 @ SET "#11=$PSCmdRange = $($PSCmdIndex + 1)..$($PSCmdContent.Length)"
 @ SET "#12=$PSCmdContent = $($PSCmdContent[$PSCmdRange] | Out-String)"
-@ SET "#13=& $([scriptblock]::Create($PSCmdContent))"
+@ SET "#13=$PSCmdDefPath = $(Join-Path Function: $env:PSCMDNAME'.pscmd')"
+@ SET "#14=Set-Content -LiteralPath $PSCmdDefPath $PSCmdContent"
+@ SET "#15=$input | & $(Split-Path $PSCmdDefPath -Leaf) %PSCMDARGS% "
 @ SET "PSCMDLINE=%#1%; %#2%; %#3%; %#4%; %#5%; %#6%; %#7%; %#8%; %#9%"
-@ SET "PSCMDLINE=%PSCMDLINE%; %#10%; %#11%; %#12%; %#13% %PSCMDARGS% "
-@ SET #1=& SET #2=& SET #3=& SET #4=& SET #5=& SET #6=& SET #7=
-@ SET #8=& SET #9=& SET #10=& SET #11=& SET #12=& SET #13=
+@ SET "PSCMDLINE=%PSCMDLINE%; %#10%; %#11%; %#12%; %#13%; %#14%; %#15%"
+@ SET #1=& SET #2=& SET #3=& SET #4=& SET #5=& SET #6=& SET #7=& SET #8=
+@ SET #9=& SET #10=& SET #11=& SET #12=& SET #13=& SET #14=& SET #15=
 @ PowerShell -Command "%PSCMDLINE%"
 @ EXIT /B %ERRORLEVEL%
 
