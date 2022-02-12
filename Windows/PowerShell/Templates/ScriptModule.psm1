4ec6feb49ab0ec27f586ee801601e3f4113834f3
天南十字星 <zjh96@live.cn>
2022-02-12 21:27:28 +0800

    Reimplemet PS script module with pipeline.

---
+++ Windows/PowerShell/Templates/ScriptModule.psm1
@@ -0,0 +1,36 @@
+﻿#.SYNOPSIS
+# 定义以脚本组成的 PowerShell 模块。
+
+Get-ChildItem $PSScriptRoot *.ps1 -File | ForEach-Object `
+{
+    $FunctionName, $ScriptPath = $_.BaseName, $_.FullName
+    $FunctionPath = Join-Path Function: $FunctionName
+    $HelpForward = ".FORWARDHELPTARGETNAME $ScriptPath"
+    $HelpCategory = ".FORWARDHELPCATEGORY ExternalScript"
+    $CommentHelp = @('<#', $HelpForward, $HelpCategory, '#>')
+    $ScriptContent = Get-Content -LiteralPath $ScriptPath -Raw
+    $ScriptAst = [scriptblock]::Create($ScriptContent).Ast
+    $UsingStatements = [string[]]$ScriptAst.UsingStatements
+    $Attributes = [string[]]$ScriptAst.ParamBlock.Attributes
+    $ParamBlock = $Attributes + [string]$ScriptAst.ParamBlock
+    if ($UsingStatements -and $ParamBlock) { $UsingStatements += @('') }
+    $FunctionHead = $UsingStatements + $ParamBlock
+    if ($FunctionHead) { $FunctionHead = @('') + $FunctionHead + @('') }
+    $QuotedScriptPath = "'$($ScriptPath.Replace("'", "''"))'"
+    $ScriptDefine = '    $Script = ' + $QuotedScriptPath
+    $CommandDefine = '    $Command = { & $Script @PSBoundParameters @args }'
+    $PipelineDefine = '    $Pipeline = $Command.GetSteppablePipeline()'
+    $DefineStatements = @($ScriptDefine, $CommandDefine, $PipelineDefine)
+    $BeginCallStatement = '    $Pipeline.Begin($PSCmdlet)'
+    $BeginStatements = $DefineStatements + $BeginCallStatement
+    $BeginBlock = @('begin', '{') + $BeginStatements + @('}')
+    $ProcessBlock = @('process', '{', '    $Pipeline.Process($_)', '}')
+    $EndBlock = @('end', '{', '    $Pipeline.End()', '}')
+    $FunctionBody = $BeginBlock + @('') + $ProcessBlock + @('') + $EndBlock
+    $FunctionContent = $CommentHelp + $FunctionHead + $FunctionBody
+    Set-Content -LiteralPath $FunctionPath ($FunctionContent | Out-String)
+    $ScriptAst.ParamBlock.Attributes |
+    Where-Object { [string]$_.TypeName -ieq 'Alias' } |
+    ForEach-Object { $_.PositionalArguments.Value } |
+    ForEach-Object { Set-Alias $_ $FunctionName }
+}
